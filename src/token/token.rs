89ae8a08bf1ab56a5b2c2fd1bb10ceb9178a54ac
vicegale <vicegale@vicegale.com>
2023-06-01 13:31:17 +0100

    removed unused code

--- src/token/token.rs
+++ src/token/token.rs
@@ -38,37 +38,3 @@ pub enum TokenKind {
     ELSE,
     RETURN
 }
-pub const ILLEGAL: &str = "ILLEGAL";
-pub const EOF: &str = "EOF";
-
-//Identifiers + Literals
-pub const IDENT: &str = "IDENT";
-pub const INT: &str = "INT";
-
-//Operators
-pub const ASSIGN: &str = "=";
-pub const PLUS: &str = "+";
-pub const MINUS: &str = "-";
-pub const BANG: &str = "!";
-pub const ASTERISK: &str = "*";
-pub const SLASH: &str = "/";
-pub const LT: &str = "<";
-pub const GT: &str = ">";
-pub const EQ: &str = "==";
-pub const NOT_EQ: &str = "!=";
-
-pub const LPAREN: &str = "(";
-pub const RPAREN: &str = ")";
-pub const LBRACE: &str = "{";
-pub const RBRACE: &str = "}";
-pub const COMMA: &str = ",";
-pub const SEMICOLON: &str = ";";
-
-//Keywords
-pub const FUNCTION: &str = "FUNCTION";
-pub const LET: &str = "LET";
-pub const TRUE: &str = "TRUE";
-pub const FALSE: &str = "FALSE";
-pub const IF: &str = "IF";
-pub const ELSE: &str = "ELSE";
-pub const RETURN: &str = "RETURN";
